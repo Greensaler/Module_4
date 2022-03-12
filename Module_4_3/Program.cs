5524c6e945a824dc1b2660e717d99e3f8e2a7d82
Greensaler <medvedev@topengineer.ru>
2022-03-12 22:20:21 +0300

    закончен 4 модуль

---
+++ Module_4_3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Module_4_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //Размерность массива 
+            var arr = new int[] { 1, 2, 3, 4 };
+            var l = arr.Length;
+
+            //Разные виды инициализации массива
+            int[] arr1 = new int[4] { 1, 2, 3, 5 };
+            int[] arr2 = new int[] { 1, 2, 3, 5 };
+            int[] arr3 = new[] { 1, 2, 3, 5 };
+            int[] arr4 = { 1, 2, 3, 5 };
+
+            // пример работы цикла foreach 
+            Console.WriteLine("Введите своё имя");
+
+            var name = Console.ReadLine();
+
+            Console.WriteLine("Ваше имя по буквам: ");
+
+            foreach (var ch in name)
+            {
+                Console.Write(ch + " ");
+            }
+
+            Console.WriteLine("Последняя буква вашего имени: {0}", name[name.Length - 1]);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Module_4_4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Module_4_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // это кортеж, можно вводить любые данные и обращаться к ним по имени
+            (string name, int age) anketa;
+            (string ; ) Pet;
+
+            Console.Write("Введите имя: ");
+            anketa.name = Console.ReadLine();
+            Console.Write("Введите возраст с цифрами: ");
+            anketa.age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ваше имя: {0}", anketa.name);
+            Console.WriteLine("Ваш возраст: {0}", anketa.age);
+
+            Console.ReadKey();
+        }
+    }
+}
