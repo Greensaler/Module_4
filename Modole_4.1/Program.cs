7eaa2627ac58676aad9b421422148930cf01a4bd
Greensaler <medvedev@topengineer.ru>
2022-03-12 17:07:17 +0300

    4.1 конец

--- Modole_4.1/Program.cs
+++ Modole_4.1/Program.cs
@@ -24,6 +24,7 @@ namespace Modole_4._1
             var C1 = (X1 < Y2) | (X1 != Y2);
             Console.WriteLine(C1);
 
+            Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
             var color = Console.ReadLine();
 
             if (color == "red")
@@ -49,7 +50,40 @@ namespace Modole_4._1
                 Console.WriteLine("Your color is cyan!");
             }
 
+            // есть полезная функция switch 
+            // break обязателен, но можно использовать 
+            // return - выход из метода
+            // goto case - позволяет перейти к другому условию
+            // throw применяется для выбора ошибок
+            // default - это если значение не соответствует ни одному из условий, но его можно пропустить
 
+            Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+
+            var color2 = Console.ReadLine();
+
+            switch (color2)
+            {
+                case "red":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is red!");
+                    break;
+
+                case "green":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is green!");
+                    break;
+
+                default:
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is cyan!");
+                    break;
+            }
 
 
         }
