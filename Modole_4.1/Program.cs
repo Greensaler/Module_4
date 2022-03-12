7dc5df25f3192c3728818918b6399dc3a5af9212
Greensaler <medvedev@topengineer.ru>
2022-03-12 12:12:41 +0300

    Изменения 4.1

--- Modole_4.1/Program.cs
+++ Modole_4.1/Program.cs
@@ -6,7 +6,21 @@ namespace Modole_4._1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string A = "1";
+            string B = "2";
+            var result = A == B;
+            Console.WriteLine(result);
+
+
+            int AA = 1 ;
+            int BB = 2;
+            double XX = 5;
+            double YY = 4;
+            var C = (AA < BB) | (XX > YY);
+            Console.WriteLine(C) ;
+            Console.ReadKey();
+           
+           
         }
     }
 }
