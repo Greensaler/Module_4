9d35029265fc9b46f86db9be91e2b066da558a1a
Greensaler <medvedev@topengineer.ru>
2022-03-12 13:57:18 +0300

    4.1

--- Modole_4.1/Program.cs
+++ Modole_4.1/Program.cs
@@ -16,8 +16,13 @@ namespace Modole_4._1
             int BB = 2;
             double XX = 5;
             double YY = 4;
-            var C = (AA < BB) | (XX > YY);
+            var C = (AA > BB) | (XX > YY);
             Console.WriteLine(C) ;
+
+            double X1 = 6;
+            double Y2 = 7;
+            var C1 = (X1 < Y2) | (X1 != Y2);
+            Console.WriteLine(C1);
             Console.ReadKey();
            
            
