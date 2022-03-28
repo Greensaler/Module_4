ee9200fe4c8dd0280223bf260b722cbc19c676ba
Greensaler <medvedev@topengineer.ru>
2022-03-28 22:16:42 +0300

    4.1.4

---
+++ Module4_1_4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Module4_1_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите переменную А");
+            string A = Console.ReadLine();
+            Console.WriteLine("Введите переменную B");
+            string B = Console.ReadLine();
+            bool C = A != B;
+            Console.WriteLine("Результат {0}", C);
+        }
+    }
+}
