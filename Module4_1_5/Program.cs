bf9e1dc42ee1ccdcf801bf19ac42e0be6a0dc0de
Greensaler <medvedev@topengineer.ru>
2022-03-28 22:57:55 +0300

    4/1/5

---
+++ Module4_1_5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Module4_1_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите переменную A, B, C, Y по порядку");
+            int A = 1;
+            int B = 3;
+            double X = 5;
+            double Y = 5;
+
+            bool C = ( A < B ) | ( X > Y );
+            Console.WriteLine("{0}", C);
+            Console.ReadKey();
+        }
+    }
+}
